4101dd194192fbfe2b7590ef9ad818effffcb7fe
castek8 <castek8@yahoo.com>
2025-07-23 23:31:21 -0400

    Add files via upload

--- tvazul/v0/back.js
+++ tvazul/v0/back.js
@@ -1,1 +1,16 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 1;
+    return needupdate;
+}
+
+function geturl() {
+    url = "https://github.com/inubyte2000/tvazul/archive/refs/tags/1.0.zip";
+    return url
+}
