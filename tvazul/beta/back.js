1b34481b2c232f2ffc3ee9a06c8835194c992bd7
castek8 <castek8@yahoo.com>
2025-07-28 18:51:52 -0400

    Add files via upload

--- tvazul/beta/back.js
+++ tvazul/beta/back.js
@@ -1,10 +1,10 @@
 ﻿function getchannels() {
     let channeldb = [
-        ["https://2-fss-1.streamhoster.com/pl_122/amlst:201814-2122456/playlist.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/create.svg"],
+        ["https://2-fss-1.streamhoster.com/pl_122/amlst:201814-2122456/playlist.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/create.png"],
         ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/newsnation.svg"],
         ["http://v2.weatherscan.net/", "https://backend-castek8.github.io/tvazul/beta/banners/weather.svg"],
-        ["https://fl3.moveonjoy.com/Start_Tv/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/start.svg"],
-        ["https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/foxsports.svg"],
+        ["https://fl3.moveonjoy.com/Start_Tv/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/start.png"],
+        ["https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/foxsports.png"],
         ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/tnt.svg"],
         ["https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/cn.png"],
         ["https://www.youtube.com/embed/SQ3nRKZ5wwA", "https://backend-castek8.github.io/tvazul/beta/banners/zen.svg"],
--- tvazul/beta/cable.js
+++ tvazul/beta/cable.js
@@ -1,11 +1,11 @@
 ﻿function getchannels() {
     let channeldb = [
         ["index.html", "NULL"],
-        ["index2.html#https://2-fss-1.streamhoster.com/pl_122/amlst:201814-2122456/playlist.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/bobross.svg"],
+        ["index2.html#https://2-fss-1.streamhoster.com/pl_122/amlst:201814-2122456/playlist.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/create.png"],
         ["index3.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/newsnation.svg"],
         ["index2.html#http://v2.weatherscan.net/", "https://backend-castek8.github.io/tvazul/beta/banners/weather.svg"],
-        ["index3.html#https://fl3.moveonjoy.com/Start_Tv/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/start.svg"],
-        ["index3.html#https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/foxsports.svg"],
+        ["index3.html#https://fl3.moveonjoy.com/Start_Tv/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/start.png"],
+        ["index3.html#https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/foxsports.png"],
         ["index3.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/tnt.svg"],
         ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/cn.png"],
         ["index2.html#https://www.youtube.com/embed/SQ3nRKZ5wwA", "https://backend-castek8.github.io/tvazul/beta/banners/zen.svg"],
