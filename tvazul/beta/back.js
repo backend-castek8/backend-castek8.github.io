1a58e48aa6200d2977750607ee0972b43399bd35
castek8 <castek8@yahoo.com>
2025-07-28 17:52:47 -0400

    Add files via upload

---
+++ tvazul/beta/back.js
@@ -0,0 +1,19 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-castek8.github.io/tvazul/beta/banners/bobross.svg"],
+        ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/newsnation.svg"],
+        ["http://v2.weatherscan.net/", "https://backend-castek8.github.io/tvazul/beta/banners/weather.svg"],
+        ["https://www.youtube.com/embed/SQ3nRKZ5wwA", "https://backend-castek8.github.io/tvazul/beta/banners/zen.svg"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvazul/beta/cable.js
@@ -0,0 +1,23 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-castek8.github.io/tvazul/beta/banners/bobross.svg"],
+        ["index3.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvazul/beta/banners/newsnation.svg"],
+        ["index2.html#http://v2.weatherscan.net/", "https://backend-castek8.github.io/tvazul/beta/banners/weather.svg"],
+        ["index2.html#https://www.youtube.com/embed/SQ3nRKZ5wwA", "https://backend-castek8.github.io/tvazul/beta/banners/zen.svg"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["ChannelName", "ChannelDescription"],
+        ["Bob Ross", "24/7 Joy of Painting"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+        ["Zen", "24/7 Zen music."],
+        ["ChannelName", "ChannelDescription"],
+    ];
+    return chinfodb;
+}
