2d35c164cfff80530f6ec74b5f94feb3cd038a80
castek8 <castek8@yahoo.com>
2025-07-28 20:17:17 -0400

    Add files via upload

---
+++ tvazul/v1/back.js
@@ -0,0 +1,31 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["https://2-fss-1.streamhoster.com/pl_122/amlst:201814-2122456/playlist.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/create.png"],
+        ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/newsnation.svg"],
+        ["http://v2.weatherscan.net/", "https://backend-castek8.github.io/tvazul/v1/banners/weather.svg"],
+        ["https://fl3.moveonjoy.com/Start_Tv/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/start.png"],
+        ["https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/foxsports.png"],
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/tnt.svg"],
+        ["https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/cn.png"],
+        ["https://www.youtube.com/embed/SQ3nRKZ5wwA", "https://backend-castek8.github.io/tvazul/v1/banners/zen.svg"],
+        ["https://streaming-live.rtp.pt/liverepeater/smil:rtpi.smil/playlist.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/rtp1.svg"],
+        ["https://cnnportugal.iol.pt/direto", "https://backend-castek8.github.io/tvazul/v1/banners/cnnpt.png"],
+        ["https://d1zx6l1dn8vaj5.cloudfront.net/out/v1/b89cc37caa6d418eb423cf092a2ef970/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/sic.png"],
+        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/633dcebd80386500074a2461/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-castek8.github.io/tvazul/v1/banners/filmelier.png"],
+        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/6824ce10c5d53e1351ceb8d1/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-castek8.github.io/tvazul/v1/banners/nick.png"],
+        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f12136385bccc00070142ed/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-castek8.github.io/tvazul/v1/banners/anime.png"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvazul/v1/cable.js
@@ -0,0 +1,47 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index3.html#https://2-fss-1.streamhoster.com/pl_122/amlst:201814-2122456/playlist.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/create.png"],
+        ["index3.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/newsnation.svg"],
+        ["index2.html#http://v2.weatherscan.net/", "https://backend-castek8.github.io/tvazul/v1/banners/weather.svg"],
+        ["index3.html#https://fl3.moveonjoy.com/Start_Tv/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/start.png"],
+        ["index3.html#https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/foxsports.png"],
+        ["index3.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/tnt.svg"],
+        ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/cn.png"],
+        ["index2.html#https://www.youtube.com/embed/SQ3nRKZ5wwA", "https://backend-castek8.github.io/tvazul/v1/banners/zen.svg"],
+        ["index3.html#https://streaming-live.rtp.pt/liverepeater/smil:rtpi.smil/playlist.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/rtp1.svg"],
+        ["index2.html#https://cnnportugal.iol.pt/direto", "https://backend-castek8.github.io/tvazul/v1/banners/cnnpt.png"],
+        ["index3.html#https://d1zx6l1dn8vaj5.cloudfront.net/out/v1/b89cc37caa6d418eb423cf092a2ef970/index.m3u8", "https://backend-castek8.github.io/tvazul/v1/banners/sic.png"],
+        ["index2.html#https://pluto.tv/br/live-tv/633dcebd80386500074a2461", "https://backend-castek8.github.io/tvazul/v1/banners/filmelier.png"],
+        ["index2.html#https://pluto.tv/br/live-tv/6824ce10c5d53e1351ceb8d1", "https://backend-castek8.github.io/tvazul/v1/banners/nick.png"],
+        ["index2.html#https://pluto.tv/br/live-tv/5f12136385bccc00070142ed", "https://backend-castek8.github.io/tvazul/v1/banners/anime.png"],
+        ["index3.html#null1", "https://backend-castek8.github.io/tvazul/v1/banners/null.png"],
+        ["index3.html#null2", "https://backend-castek8.github.io/tvazul/v1/banners/null.png"].
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["ChannelName", "ChannelDescription"],
+        ["Create", "General content"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+        ["Start TV", "TV Shows"],
+        ["Fox Sports", "24/7 Sports coverage"],
+        ["TNT", "Movies all the time."],
+        ["Cartoon Network", "Cartoons for all ages"],
+        ["Zen", "24/7 Zen music."],
+        ["RTP 1", "Conteúdo geral"],
+        ["CNN Portugal", "Cobertura noticiosa 24-7"],
+        ["SIC", "Tele novelas"],
+        ["SIC", "Filmes Canal"],
+        ["Nick", "Desenhos animados para todas as idades"],
+        ["Anime", "Anime para todas as idades"],
+        ["Channel", "Description not available"],
+        ["Channel", "Description not available"],
+        ["ChannelName", "ChannelDescription"],
+    ];
+    return chinfodb;
+}
