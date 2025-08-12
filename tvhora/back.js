3e171382bbb34e49e69c65bc489f3e3959931539
castek8 <castek8@yahoo.com>
2025-08-11 21:49:23 -0400

    Add files via upload

--- tvhora/back.js
+++ tvhora/back.js
@@ -1,1 +1,33 @@
-#add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["POOF", "POOF"],
+        ["#", "nochan.png"],
+        ["https://service-stitcher.clusters.pluto.tv/v2/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-castek8.github.io/tvhora/ch2.png"],
+        ["http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvhora/ch3.png"],
+        ["http://v1.weatherscan.net/", "https://backend-castek8.github.io/tvhora/ch4.png"],
+        ["https://fl3.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-castek8.github.io/tvhora/ch5.png"],
+        ["https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/tvhora/ch6.png"],
+        ["https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-gf2dVw-playlist.m3u8", "https://backend-castek8.github.io/tvhora/ch7.png"],
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/tvhora/ch8.png"],
+        ["https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvhora/ch9.png"],
+        ["https://www.rtvs.sk/embed/live/1", "https://backend-castek8.github.io/tvhora/ch10.png"],
+        ["https://www.rtvs.sk/embed/live/2", "https://backend-castek8.github.io/tvhora/ch11.png"],
+        ["https://www.rtvs.sk/embed/live/3", "https://backend-castek8.github.io/tvhora/ch12.png"],
+        ["https://www.rtvs.sk/embed/live/15", "https://backend-castek8.github.io/tvhora/ch13.png"],
+        ["https://live.joj.sk/", "https://backend-castek8.github.io/tvhora/ch14.png"],
+        ["https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://backend-castek8.github.io/tvhora/ch15.png"],
+        ["https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://backend-castek8.github.io/tvhora/ch16.png"],
+        ["https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://backend-castek8.github.io/tvhora/ch17.png"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvhora/cable.js
@@ -0,0 +1,49 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-castek8.github.io/tvhora/ch2.png"],
+        ["index3.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/tvhora/ch3.png"],
+        ["index2.html#http://v1.weatherscan.net/", "https://backend-castek8.github.io/tvhora/ch4.png"],
+        ["index3.html#https://fl3.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-castek8.github.io/tvhora/ch5.png"],
+        ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/tvhora/ch6.png"],
+        ["index3.html#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-castek8.github.io/tvhora/ch7.png"],
+        ["index3.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/tvhora/ch8.png"],
+        ["index3.html#https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/tvhora/ch9.png"],
+        ["index2.html#https://www.rtvs.sk/embed/live/1", "https://backend-castek8.github.io/tvhora/ch10.png"],
+        ["index2.html#https://www.rtvs.sk/embed/live/2", "https://backend-castek8.github.io/tvhora/ch11.png"],
+        ["index2.html#https://www.rtvs.sk/embed/live/3", "https://backend-castek8.github.io/tvhora/ch12.png"],
+        ["index2.html#https://www.rtvs.sk/embed/live/15", "https://backend-castek8.github.io/tvhora/ch13.png"],
+        ["index2.html#https://live.joj.sk/", "https://backend-castek8.github.io/tvhora/ch14.png"],
+        ["index2.html#https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://backend-castek8.github.io/tvhora/ch15.png"],
+        ["index2.html#https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://backend-castek8.github.io/tvhora/ch16.png"],
+        ["index2.html#https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://backend-castek8.github.io/tvhora/ch17.png"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Bob Ross", "24/7 Joy of Painting"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+        ["E!", "The most popular movies and TV shows"],
+        ["Cartoon Network", "24/7 cartoons"],
+        ["Toonami", "24/7 Cartoons and Anime"],
+        ["TNT", "Movies all the time."],
+        ["Fox Sports", "24/7 Sports coverage"],
+        ["RTVS 1", "Všeobecný obsah"],
+        ["RTVS 2", "Všeobecný a vzdelávací obsah"],
+        ["RTVS 24", "Správy 24 hodín denne, 7 dní v týždni"],
+        ["RTVS SPORT", "Šport 24 hodín denne, 7 dní v týždni"],
+        ["JOJ", "Televízne programy a filmy"],
+        ["Marzika", "Rodinný obsah"],
+        ["Doma", "Televízne relácie pre ženy"],
+        ["Dajto", "Televízne relácie pre mužov"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
