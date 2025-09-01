73d064a98da171b7445363f64e53f60d083e4d76
castek8 <castek8@yahoo.com>
2025-08-31 22:27:32 -0400

    Add files via upload

--- tvhora/back.js
+++ tvhora/back.js
@@ -3,13 +3,13 @@
         ["index.html", "NULL"],
         ["index.html", "NULL"],
         ["https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-castek8.github.io/meiji/ch2.svg"],
-        ["index2.htm#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/meiji/ch3.svg"],
+        ["index2.html#http://fl4.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-castek8.github.io/meiji/ch3.svg"],
         ["http://v1.weatherscan.net/", "https://backend-castek8.github.io/meiji/ch4.svg"],
-        ["index2.htm#https://fl3.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-castek8.github.io/meiji/ch5.svg"],
-        ["index2.htm#https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/meiji/ch6.png"],
-        ["index2.htm#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-castek8.github.io/meiji/ch7.png"],
-        ["index2.htm#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/meiji/ch8.svg"],
-        ["index2.htm#https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/meiji/ch9.png"],
+        ["index2.html#https://fl3.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-castek8.github.io/meiji/ch5.svg"],
+        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-castek8.github.io/meiji/ch6.png"],
+        ["index2.html#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-castek8.github.io/meiji/ch7.png"],
+        ["index2.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-castek8.github.io/meiji/ch8.svg"],
+        ["index2.html#https://fl3.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-castek8.github.io/meiji/ch9.png"],
     ];
     return channeldb;
 }
